7b72aceb5facdd726ee245c4e9742f5a702b32e3
doppelgangerlynx <bags8040@gmail.com>
2019-11-23 05:43:21 +0000

    Also updated nvcc_plugin and setup for v3

--- nvcc_plugin.py
+++ nvcc_plugin.py
@@ -1,5 +1,6 @@
 from v1.v1 import NVCCPlugin as NVCC_V1
 from v2.v2 import NVCCPluginV2 as NVCC_V2
+from v3.v3 import NVCCPluginV3 as NVCC_V3
 
 
 def load_ipython_extension(ip):
@@ -8,3 +9,6 @@ def load_ipython_extension(ip):
 
     nvcc_plugin_v2 = NVCC_V2(ip)
     ip.register_magics(nvcc_plugin_v2)
+    
+    nvcc_plugin_v3 = NVCC_V3(ip)
+    ip.register_magics(nvcc_plugin_v3)
